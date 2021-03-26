5417697c336e0a6310288a96549da21216e1bdab
Jeevan0604 <jeevankumartm06042002@gmail.com>
2021-03-26 14:25:56 +0000

    ok

--- Program-2.c
+++ Program-2.c
@@ -13,7 +13,7 @@ int main()
 {
 	char str1[50], str2[50];
 	int i;
-	printf("Enter a string-1: ");
+	printf("Enter a string-1:");
 	scanf("%s",str1);
 	for (i=0;str1[i]!='\0';i++)                  //Complete the code in for-loop & copy logic
   {                          
